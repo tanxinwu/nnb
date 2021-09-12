face6ea5525d36a9450a0dad0e230d454d0c18f2
lebron <1227781351@qq.com>
2021-09-12 16:29:16 +0800

    新增删除和获取接口

--- config/src/main/java/com/base/config/controller/BaseController.java
+++ config/src/main/java/com/base/config/controller/BaseController.java
@@ -1,14 +1,25 @@
 package com.base.config.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("test")
 public class BaseController {
 
 
-    @PostMapping("test")
+    @PostMapping()
     public String test(){
         return "testjson";
+    }
+
+
+    @GetMapping()
+    public String getTest(){
+        return "";
+    }
+
+    @DeleteMapping("/{id}")
+    public int deleteTest(@PathVariable("id") Integer id){
+        return 0;
     }
 }
