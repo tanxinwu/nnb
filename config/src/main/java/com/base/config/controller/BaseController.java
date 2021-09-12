716b24661f86360a9a5a14a407340cc90c08b3a9
lebron <1227781351@qq.com>
2021-09-12 16:05:51 +0800

    增加了test接口

--- config/src/main/java/com/base/config/controller/BaseController.java
+++ config/src/main/java/com/base/config/controller/BaseController.java
@@ -1,7 +1,14 @@
 package com.base.config.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
+
+
+    @PostMapping("test")
+    public String test(){
+        return "testjson";
+    }
 }
